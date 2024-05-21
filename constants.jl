2ac9a6833fe11960edac2a4939ca589cc0e39e04
chinchay <35080984+chinchay@users.noreply.github.com>
2024-05-21 09:06:29 -0300

    feat: Introduce Lennard Jones force

--- constants.jl
+++ constants.jl
@@ -1,5 +1,11 @@
 module ConstantsModule
 
-DT = 0.1
+DT = 0.01
+
+s = 1
+ϵ = 2
+s6 = s ^ 6
+s12 = s6 ^ 2
+rMin = (2 ^ (1 / 6)) * s 
 
 end
--- dynamics.jl
+++ dynamics.jl
@@ -5,21 +5,43 @@
 
 module DynamicsModule
 
-using LinearAlgebra # functions: norm
-
 include("atom.jl")
+include("constants.jl")
+
+import .ConstantsModule
+import .ConstantsModule: s, ϵ, s6, s12
+using LinearAlgebra # functions: norm
 
 import ..AtomModule: Atom
 
-export updateAccelarationLJ
+export updateHarmonicAccelaration, updateLennarJonesAcceleration
 
-function updateAccelarationLJ(atom1::Atom, atom2::Atom)
+function computeDistance(atom1::Atom, atom2::Atom)
     dR21 = atom2._r - atom1._r
-    # dR12 = -dR21
-
     distance = norm(dR21)
+    return dR21, distance
+end
+
+function computeLennardJonesForce(distance::Float64)
+    repulsive = s12 / (distance ^ 13)
+    attractive = 0.5 * s6 / (distance ^ 7)
+    force = 48 * ϵ * (repulsive - attractive)
+    return force
+end
+
+function updateLennarJonesAcceleration(atom1::Atom, atom2::Atom)
+    dR21, distance = computeDistance(atom1, atom2)
+    absForce = computeLennardJonesForce(distance)
+
     u21 = dR21 / distance
-    # u12 = -u21
+    force21 = absForce * u21
+    force12 = -force21
+    atom2._a = force21 / atom2.mass
+    atom1._a = force12 / atom1.mass
+end
+
+function updateHarmonicAccelaration(atom1::Atom, atom2::Atom)
+    dR21, distance = computeDistance(atom1, atom2)
 
     factor = -1
     force21 = factor * dR21
